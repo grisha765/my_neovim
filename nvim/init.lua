e5badb2675fadafd9c2ef9323160a8c02ef6bced
Grisha <you@example.com>
2024-05-28 23:35:58 +0300

    [u] update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -63,12 +63,17 @@ local function git_commit_push(commit_message)
   local git_add_cmd = "git add " .. vim.fn.shellescape(current_file)
   local add_result = vim.fn.systemlist(git_add_cmd)
 
+  -- Получение текущей ветки
+  local git_branch_cmd = "git rev-parse --abbrev-ref HEAD"
+  local branch_result = vim.fn.systemlist(git_branch_cmd)
+  local current_branch = branch_result[1]
+
   -- Команда git commit
   local git_commit_cmd = "git commit -m " .. vim.fn.shellescape(commit_message)
   local commit_result = vim.fn.systemlist(git_commit_cmd)
 
   -- Команда git push
-  local git_push_cmd = "git push origin main"
+  local git_push_cmd = "git push origin " .. current_branch
   local push_result = vim.fn.systemlist(git_push_cmd)
 
   -- Вывод результата в командной строке
@@ -94,6 +99,8 @@ end, { nargs = 1 })
 -- Переменная для хранения окна терминала
 _G.terminal_win = nil
 _G.terminal_buf = nil
+_G.previous_win = nil
+_G.is_terminal_focused = false
 
 -- Функция для открытия или скрытия терминала
 function _G.toggle_terminal()
@@ -132,11 +139,29 @@ function _G.toggle_terminal()
   end
 end
 
+-- Функция для проверки фокуса терминала
+function _G.check_terminal_focus()
+  if vim.api.nvim_get_current_win() == _G.terminal_win and not _G.is_terminal_focused then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i', true, false, true), 'n', true)
+    _G.is_terminal_focused = true
+  elseif vim.api.nvim_get_current_win() ~= _G.terminal_win then
+    _G.is_terminal_focused = false
+  end
+end
+
 -- Привязываем функцию к клавише "t" в Normal режиме
 vim.api.nvim_set_keymap('n', '<C-t>', ':lua toggle_terminal()<CR>', { noremap = true, silent = true })
 
 -- Привязываем функцию к Ctrl + t в Terminal режиме
 vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n>:lua toggle_terminal()<CR>', { noremap = true, silent = true })
+
+-- Автокоманда для переключения в режим вставки при входе в окно терминала
+vim.api.nvim_exec([[
+  augroup TerminalFocus
+    autocmd!
+    autocmd WinEnter * lua check_terminal_focus()
+  augroup END
+]], false)
 
 -- Опционально, можно настроить автокоманду для закрытия терминала, когда он не нужен
 vim.api.nvim_exec([[

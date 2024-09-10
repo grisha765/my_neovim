b0851a90ce083ce7b746823fbff0c8ced96cc598
Grisha <grisha765@tuta.io>
2024-09-10 20:00:11 +0300

    [f] fix autocomplete enter

--- nvim/init.lua
+++ nvim/init.lua
@@ -639,7 +639,13 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        fallback()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()

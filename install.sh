7d9f374701602e0438300098a334f4b8e4da147f
Grisha <grisha765@tuta.io>
2025-01-28 04:20:23 +0300

    [+] add install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+base_url="https://raw.githubusercontent.com/grisha765/my_neovim/main"
+
+mkdir -p ~/.config/nvim/lua
+mkdir -p ~/.config/lazygit
+
+wget -O ~/.config/nvim/init.lua $base_url/nvim/init.lua
+wget -O ~/.config/lazygit/config.yml $base_url/lazygit/config.yml
+
+lua_modules=(
+  "funcs.lua"
+  "gpt.lua"
+  "highlight.lua"
+  "plugins.lua"
+  "settings.lua"
+  "tabs.lua"
+  "terminal.lua"
+  )
+
+for file in "${lua_modules[@]}"; do
+  wget -O ~/.config/nvim/lua/${file} $base_url/nvim/lua/${file}
+done
+
+nvim

1f990d0ad3fe463da205eb6de8d0caa7b649e9aa
Grisha <grisha765@tuta.io>
2025-04-06 05:32:17 +0300

    [u] for easy development

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+script_dir=$(dirname "$(realpath "$BASH_SOURCE")")
+
 base_url="https://raw.githubusercontent.com/grisha765/my_neovim/main"
 
 mkdir -p ~/.config/nvim/lua
 mkdir -p ~/.config/lazygit
 
-wget -O ~/.config/nvim/init.lua $base_url/nvim/init.lua
-wget -O ~/.config/lazygit/config.yml $base_url/lazygit/config.yml
+if [ -d "$script_dir/.git" ]; then
+  ln -sf $script_dir/nvim/init.lua ~/.config/nvim/init.lua
+  ln -sf $script_dir/lazygit/config.yml ~/.config/lazygit/config.yml
+else
+  wget -O ~/.config/nvim/init.lua $base_url/nvim/init.lua
+  wget -O ~/.config/lazygit/config.yml $base_url/lazygit/config.yml
+fi
 
 lua_modules=(
   "funcs.lua"
@@ -20,7 +27,11 @@ lua_modules=(
   )
 
 for file in "${lua_modules[@]}"; do
-  wget -O ~/.config/nvim/lua/${file} $base_url/nvim/lua/${file}
+  if [ -d "$script_dir/.git" ]; then
+    ln -sf $script_dir/nvim/lua/${file} ~/.config/nvim/lua/${file}
+  else
+    wget -O ~/.config/nvim/lua/${file} $base_url/nvim/lua/${file}
+  fi
 done
 
 nvim

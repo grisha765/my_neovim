14df65663e54dbb4fabcdc5f14bea57e61708e4d
Grisha <grisha765@tuta.io>
2025-04-06 05:15:50 +0300

    [u] hiding terminal when editing

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -1,73 +1,103 @@
--- Переменная для хранения окна терминала
+-- Эти переменные нужны, чтобы хранить окно терминала, буфер и предыдущее окно
 _G.terminal_win = nil
 _G.terminal_buf = nil
 _G.previous_win = nil
-_G.is_terminal_focused = false
 
--- Функция для открытия или скрытия терминала
-function _G.toggle_terminal()
-  if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
-    vim.api.nvim_set_current_win(_G.previous_win)
-    vim.api.nvim_win_hide(terminal_win)
-    terminal_win = nil
+-- Открыть терминал (если он уже открыт, просто фокусируемся на нём)
+local function open_terminal()
+  if _G.terminal_win and vim.api.nvim_win_is_valid(_G.terminal_win) then
+    vim.api.nvim_set_current_win(_G.terminal_win)
+    return
+  end
+
+  _G.previous_win = vim.api.nvim_get_current_win()
+
+  if _G.terminal_buf and vim.api.nvim_buf_is_valid(_G.terminal_buf) then
+    vim.cmd('botright split')
+    vim.cmd('resize 10')
+    vim.api.nvim_set_current_buf(_G.terminal_buf)
+    _G.terminal_win = vim.api.nvim_get_current_win()
   else
-    _G.previous_win = vim.api.nvim_get_current_win()
+    vim.cmd('botright split')
+    vim.cmd('resize 10')
+    vim.cmd('terminal')
+    _G.terminal_win = vim.api.nvim_get_current_win()
+    _G.terminal_buf = vim.api.nvim_get_current_buf()
+  end
 
-    if _G.terminal_buf and vim.api.nvim_buf_is_valid(_G.terminal_buf) then
-      vim.cmd('botright split')
-      vim.cmd('resize 10')
-      vim.api.nvim_set_current_buf(_G.terminal_buf)
-      terminal_win = vim.api.nvim_get_current_win()
-    else
-      vim.cmd('botright split')
-      vim.cmd('resize 10')
-      vim.cmd('terminal')
-      terminal_win = vim.api.nvim_get_current_win()
-      _G.terminal_buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_win_set_option(_G.terminal_win, 'number', false)
+  vim.api.nvim_win_set_option(_G.terminal_win, 'relativenumber', false)
+
+  vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes('i', true, false, true),
+    'n',
+    true
+  )
+end
+
+-- Скрыть терминал (если он вообще открыт)
+local function close_terminal()
+  if
+    _G.terminal_win
+    and type(_G.terminal_win) == 'number'
+    and vim.api.nvim_win_is_valid(_G.terminal_win)
+  then
+    vim.api.nvim_win_hide(_G.terminal_win)
+  end
+  _G.terminal_win = nil
+end
+
+-- Основная функция переключения терминала (открыть/скрыть)
+function _G.toggle_terminal()
+  if
+    _G.terminal_win
+    and type(_G.terminal_win) == 'number'
+    and vim.api.nvim_win_is_valid(_G.terminal_win)
+  then
+    close_terminal()
+    if
+      _G.previous_win
+      and vim.api.nvim_win_is_valid(_G.previous_win)
+    then
+      vim.api.nvim_set_current_win(_G.previous_win)
     end
-    vim.api.nvim_win_set_option(terminal_win, 'number', false)
-    vim.api.nvim_win_set_option(terminal_win, 'relativenumber', false)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i', true, false, true), 'n', true)
+  else
+    -- Иначе открываем
+    open_terminal()
   end
 end
 
--- Функция для проверки фокуса терминала
-function _G.check_terminal_focus()
-  if vim.api.nvim_get_current_win() == _G.terminal_win and not _G.is_terminal_focused then
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i', true, false, true), 'n', true)
-    _G.is_terminal_focused = true
-  elseif vim.api.nvim_get_current_win() ~= _G.terminal_win then
-    _G.is_terminal_focused = false
+-- Функция, которая будет автоматически скрывать терминал,
+-- если мы переключаемся в другое окно
+function _G.auto_hide_terminal_on_focus_lost()
+  local current_win = vim.api.nvim_get_current_win()
+  if
+    _G.terminal_win
+    and type(_G.terminal_win) == 'number'
+    and vim.api.nvim_win_is_valid(_G.terminal_win)
+  then
+    if current_win ~= _G.terminal_win then
+      close_terminal()
+      if
+        _G.previous_win
+        and vim.api.nvim_win_is_valid(_G.previous_win)
+        and current_win ~= _G.previous_win
+      then
+        vim.api.nvim_set_current_win(_G.previous_win)
+      end
+    end
   end
 end
 
--- Привязываем функцию к клавише "t" в Normal режиме
+-- Шорткаты для переключения терминала в Normal- и Terminal-режиме
 vim.api.nvim_set_keymap('n', '<C-t>', ':lua toggle_terminal()<CR>', { noremap = true, silent = true })
-
--- Привязываем функцию к Ctrl + t в Terminal режиме
 vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n>:lua toggle_terminal()<CR>', { noremap = true, silent = true })
 
--- Автокоманда для переключения в режим вставки при входе в окно терминала
+-- Автокоманда: на каждом WinEnter проверяем, вышли ли мы из терминала
 vim.api.nvim_exec([[
-  augroup TerminalFocus
+  augroup TerminalAutoHide
     autocmd!
-    autocmd WinEnter * lua check_terminal_focus()
-  augroup END
-]], false)
-
--- Функция для скрытия терминала при переключении вкладок
-function _G.hide_terminal_on_tab_change()
-  if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
-    vim.api.nvim_win_hide(terminal_win)
-    terminal_win = nil
-  end
-end
-
--- Автокоманда для скрытия терминала при переключении вкладок
-vim.api.nvim_exec([[
-  augroup TerminalHideOnTabChange
-    autocmd!
-    autocmd TabEnter * lua hide_terminal_on_tab_change()
+    autocmd WinEnter * lua auto_hide_terminal_on_focus_lost()
   augroup END
 ]], false)
 

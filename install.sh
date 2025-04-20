4e623db423e4d953949832843c692379f4a8be2c
Grisha <grisha765@tuta.io>
2025-04-20 04:09:22 +0300

    [u] update install script

--- install.sh
+++ install.sh
@@ -1,19 +1,52 @@
 #!/usr/bin/env bash
 
-script_dir=$(dirname "$(realpath "$BASH_SOURCE")")
+set -e
 
-base_url="https://raw.githubusercontent.com/grisha765/my_neovim/main"
+if [[ -n "$BASH_SOURCE" && "$BASH_SOURCE" != "/dev/stdin" ]]; then
+    script_path="$BASH_SOURCE"
+elif [[ -n "$0" && "$0" != "bash" ]]; then
+    script_path="$0"
+else
+    script_path=""
+fi
 
-mkdir -p ~/.config/nvim/lua
-mkdir -p ~/.config/lazygit
+if [[ -n "$script_path" ]]; then
+    script_dir=$(dirname "$(realpath "$script_path")")
+else
+    script_dir=$(pwd)
+fi
 
 if [ -d "$script_dir/.git" ]; then
-  ln -sf $script_dir/nvim/init.lua ~/.config/nvim/init.lua
-  ln -sf $script_dir/lazygit/config.yml ~/.config/lazygit/config.yml
-else
-  wget -O ~/.config/nvim/init.lua $base_url/nvim/init.lua
-  wget -O ~/.config/lazygit/config.yml $base_url/lazygit/config.yml
+  echo "Script dir: $script_dir"
+  expected_url="github.com:grisha765/my_neovim.git"
+  remote_short_url=""
+  if git -C "$script_dir" remote get-url origin &>/dev/null; then
+    remote_url="$(git -C "$script_dir" remote get-url origin)"
+    remote_short_url="$(echo "$remote_url" | sed -E 's#(git@|https://)github\.com(:|/)#github.com:#')"
+  fi
 fi
+
+use_local_repo=false
+if [ -d "$script_dir/.git" ] && [ "$remote_short_url" = "$expected_url" ]; then
+  use_local_repo=true
+fi
+
+if ! $use_local_repo; then
+  if command -v wget >/dev/null 2>&1; then
+    download_cmd='wget -q -O'
+  elif command -v curl >/dev/null 2>&1; then
+    download_cmd='curl -sSf -o'
+  else
+    echo "Error: wget or curl not found." >&2
+    exit 1
+  fi
+fi
+
+base_config="$HOME/.config"
+base_url="https://raw.githubusercontent.com/grisha765/my_neovim/main"
+
+mkdir -pv ~/.config/nvim/lua
+mkdir -pv ~/.config/lazygit
 
 lua_modules=(
   "funcs.lua"
@@ -26,11 +59,22 @@ lua_modules=(
   "autosave.lua"
   )
 
-for file in "${lua_modules[@]}"; do
-  if [ -d "$script_dir/.git" ]; then
-    ln -sf $script_dir/nvim/lua/${file} ~/.config/nvim/lua/${file}
+if [ -d "$script_dir/.git" ]; then
+  ln -sfv "$script_dir/nvim/init.lua" "$base_config/nvim/init.lua"
+  ln -sfv "$script_dir/lazygit/config.yml" "$base_config/lazygit/config.yml"
+else
+  echo "Download init.lua..."
+  $download_cmd "$base_config/nvim/init.lua" "$base_url/nvim/init.lua"
+  echo "Download config.yml..."
+  $download_cmd "$base_config/lazygit/config.yml" "$base_url/lazygit/config.yml"
+fi
+
+for i in "${lua_modules[@]}"; do
+  if $use_local_repo; then
+    ln -sfv "$script_dir/nvim/lua/$i" "$base_config/nvim/lua/$i"
   else
-    wget -O ~/.config/nvim/lua/${file} $base_url/nvim/lua/${file}
+    echo "Download $i..."
+    $download_cmd "$base_config/nvim/lua/$i" "$base_url/nvim/lua/$i"
   fi
 done
 
